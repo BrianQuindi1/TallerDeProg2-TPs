7b508bc24b0d55df4306327df1e9c71cd3e9a8c9
Brian Isaac Quindi <brianquindi1@gmail.com>
2025-06-25 19:38:50 -0300

    agregado de tps 2 y 3

---
+++ Tp2-TPr2/mac.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+fs.readFile('package.json', 'utf8', (err, contenido) => {
+    if (err) return console.error('Error:', err.message);
+    
+    fs.stat('package.json', (err, stats) => {
+        if (err) return console.error('Error:', err.message);
+        
+        const info = {
+            contenidoStr: contenido,
+            contenidoObj: JSON.parse(contenido),
+            size: stats.size
+        };
+        console.log(info);
+        
+        fs.writeFile('info.txt', JSON.stringify(info, null, '\t'), (err) => {
+            if (err) console.error('Error:', err.message);
+        });
+    });
+});
